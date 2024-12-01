c056ba63708988a8859d954671da9110befb5df8
aimane <120175452+rhiminiaimane@users.noreply.github.com>
2024-12-01 20:25:02 +0100

    static/css

--- DataLab/settings.py
+++ DataLab/settings.py
@@ -117,7 +117,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = '/static/'  # URL to access static files
+
+# Specify where Django looks for static files during development
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Points to the 'static' directory in your project
+]
+
+# Define a root directory to collect static files for production
+STATIC_ROOT = BASE_DIR / "staticfiles"  # Where 'collectstatic' will gather files
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- app/urls.py
+++ app/urls.py
@@ -6,14 +6,9 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.page1, name = 'page1'),
     path('home/', views.home_page, name='home'),
-
     path("accounts/", include("django.contrib.auth.urls")),
-
     path('signup/', views.authView, name='authView'),
-
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-
-
     path('upload_and_display/', views.upload_and_display, name='upload_and_display'),
     path('visualize/', views.visualize, name='visualize'),
 ]
